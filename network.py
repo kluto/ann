1c80f6da696b488e6e1a5e80d7491a0a236646ea
tpk@tb3 <tpkludka@gmail.com>
2017-07-08 01:06:13 +0100

    simple network implemented

--- network.py
+++ network.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class network():
+class Network():
     def __init__(self):
         # Network architecture
         self.s_input = 2
@@ -10,7 +10,28 @@ class network():
         self.W1 = np.random.randn(self.s_input, self.s_hidden1)
         self.W2 = np.random.randn(self.s_hidden1, self.s_output)
 
-    def fwd_prop(self, N):
+    def sigmoid(self, z, deriv=False):
+        if deriv:
+            return self.sigmoid(z)*(1-self.sigmoid(z))
+        return 1/(1+np.exp(-z))
+
+    def cost(self, x, y):
+        self.y_hat = self.feed(x)
+        return 0.5*sum((y - self.y_hat)**2)
+        
+    def backprop(self, x, y):
+        self.y_hat = self.feed(x)
+        # output layer error                    
+        d_3 = -(y - self.y_hat) * self.sigmoid(self.z3, deriv=True)
+        # propagate to layer2 weigths
+        grad_W2 = np.dot(self.a2.T, d_3)
+        # layer2 error 
+        d_2 = np.dot(d_3, self.W2.T) * self.sigmoid(self.z2, deriv=True)
+        # propagate to input layer
+        grad_W1 = np.dot(x.T, d_2)
+        return grad_W1, grad_W2
+    
+    def feed(self, N):
         # hidden layer activity
         self.z2 = np.dot(N, self.W1)
         # hidden layer activation
@@ -20,8 +41,22 @@ class network():
         # output activity
         a3 = self.sigmoid(self.z3)
         return a3
-        
-    def sigmoid(self, z, deriv=False):
-        if deriv:
-            return self.sigmoid(z)*(1-self.sigmoid(z))
-        return 1/(1+np.exp(-z))
+
+# x = (hours sleeping, hours studying), y = Score on test
+x = np.array([[3, 5], [5, 1], [10, 2]], dtype=float)
+y = np.array([[75], [82], [93]], dtype=float)
+
+x = x/np.amax(x, axis=0)
+y = y/100
+scalar = 3
+
+NN = Network()
+print('NN initialised with cost:', NN.cost(x, y))
+for _ in range(100):
+    dJdW1, dJdW2 = NN.backprop(x, y)
+    NN.W1 = NN.W1 - scalar * dJdW1
+    NN.W2 = NN.W2 - scalar * dJdW2
+    print(NN.cost(x, y))
+
+
+
