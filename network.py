4d0d318baee78b618a48761badacaaeee800343e
tpk@tb1 <tpkludka@gmail.com>
2017-07-11 14:22:39 +0100

    clean up

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import optimize
 
 class Network():
     def __init__(self):
@@ -15,12 +16,12 @@ class Network():
             return self.sigmoid(z)*(1-self.sigmoid(z))
         return 1/(1+np.exp(-z))
 
-    def cost(self, x, y):
-        self.y_hat = self.feed(x)
+    def cost(self, X, y):
+        self.y_hat = self.feed(X)
         return 0.5*sum((y - self.y_hat)**2)
         
-    def backprop(self, x, y):
-        self.y_hat = self.feed(x)
+    def backprop(self, X, y):
+        self.y_hat = self.feed(X)
         # output layer error                    
         d_3 = -(y - self.y_hat) * self.sigmoid(self.z3, deriv=True)
         # propagate to layer2 weigths
@@ -28,7 +29,7 @@ class Network():
         # layer2 error 
         d_2 = np.dot(d_3, self.W2.T) * self.sigmoid(self.z2, deriv=True)
         # propagate to input layer
-        grad_W1 = np.dot(x.T, d_2)
+        grad_W1 = np.dot(X.T, d_2)
         return grad_W1, grad_W2
     
     def feed(self, N):
@@ -41,6 +42,33 @@ class Network():
         # output activity
         a3 = self.sigmoid(self.z3)
         return a3
+
+    def get_params(self):
+        #Get W1 and W2 unrolled into vector:
+        params = np.concatenate((self.W1.ravel(), self.W2.ravel()))
+        return params
+    
+
+class Trainer():
+    def __init__(self, N):
+        self.N = N
+
+
+    def train(self, X, y):
+        self.X = X
+        self.y = y
+        self.J = []
+        
+        params0 = self.N.getParams()
+
+        options = {'maxiter': 200, 'disp' : True}
+        _res = optimize.minimize(self.costFunctionWrapper, params0, jac=True, method='BFGS', \
+                                 args=(X, y), options=options, callback=self.callbackF)
+
+        self.N.setParams(_res.x)
+        self.optimizationResults = _res
+
+
 
 # x = (hours sleeping, hours studying), y = Score on test
 x = np.array([[3, 5], [5, 1], [10, 2]], dtype=float)
